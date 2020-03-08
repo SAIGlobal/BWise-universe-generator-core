b8bc3b0d473458b46400a9f30c92ef2be21b0d1e
guigui42 <guigui42@users.noreply.github.com>
2020-03-08 10:01:27 +0100

    switch to 64bits

--- config/eUniverseConf.bat
+++ config/eUniverseConf.bat
@@ -1,9 +1,16 @@
 echo Starting %*
 @echo off
+
+set CUSTOMJAVAPATH=C:\Program Files\Java\jre1.8.0_241
+IF "%JAVAPATH%"=="" set JAVA_HOME=%CUSTOMJAVAPATH%
+
+java -version
+
 FOR /F "skip=2 tokens=2,*" %%A IN ('REG.exe query "HKEY_LOCAL_MACHINE\SOFTWARE\SAP BusinessObjects\Suite XI 4.0\Shared" /v "CommonFiles"') DO set "InstallPath=%%B"
-set SAPBOInstallPath=%InstallPath%win32_x86
+set SAPBOInstallPath=%InstallPath%win64_x64
 echo %SAPBOInstallPath%
 
-SET PATH=%SAPBOInstallPath%;%PATH%
 
-java -d32 -Dbusinessobjects.connectivity.directory="%InstallPath%\dataAccess\connectionServer" -jar eUniverse.jar -conf eUniverse.ini
+SET PATH=%JAVA_HOME%\bin;%SAPBOInstallPath%;%PATH%
+
+"%JAVA_HOME%\bin\java.exe"   -Dbusinessobjects.connectivity.directory="%InstallPath%\dataAccess\connectionServer" -cp eUniverse.jar;"%InstallPath%\\java\\lib/*" com.bwise.eUniverse.Main -conf %1 
